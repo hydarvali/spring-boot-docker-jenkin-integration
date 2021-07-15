61fc4b73a677f2faf2f23b5157cb945ecd2c8206
hydar <valisyed34@gmail.com>
2021-07-15 21:11:54 +0530

    Jenkin and Docker integration

---
+++ src/main/java/com/docker/jenkin/demo/SpringBootDockerJenkinIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.docker.jenkin.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerJenkinIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerJenkinIntegrationApplication.class, args);
+	}
+
+}
